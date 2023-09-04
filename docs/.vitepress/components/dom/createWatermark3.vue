bd8564c5d9ac16b11a433d255dd722b33a088f88
Shopping <xpzheng6@163.com>
2023-09-04 22:40:53 +0800

    feat: dom, update watermark demo

--- docs/.vitepress/components/dom/createWatermark3.vue
+++ docs/.vitepress/components/dom/createWatermark3.vue
@@ -5,34 +5,31 @@
 </template>
 
 <script setup lang="ts">
-import { createWatermark } from '@shoppingzh/tools/lib/dom'
+import { createWatermark, loadImage } from '@shoppingzh/tools/lib/dom'
 import { onMounted, ref } from 'vue';
 import bg from './bg.jpg'
 
 const containerEl = ref<HTMLElement>()
 const el = ref<HTMLCanvasElement>()
 
-function render() {
+async function render() {
   el.value.width = containerEl.value.clientWidth
 
   const ctx = el.value.getContext('2d')
   ctx.fillRect(0, 0, el.value.width, el.value.height)
-  const img = new Image()
-  img.src = bg
 
-  img.onload = () => {
-    ctx.drawImage(img, 0, 0)
+  const img = await loadImage(bg)
 
-    createWatermark(el.value, '我是图片水印', {
-      textColor: 'green',
-      textSize: 20,
-      bgColor: 'rgba(0, 0, 0, .1)',
-      fontBold: true,
-      rotate: 30,
-      padding: [100, 100],
-    })
-  }
+  ctx.drawImage(img, 0, 0)
 
+  createWatermark(el.value, '我是图片水印', {
+    textColor: 'green',
+    textSize: 20,
+    bgColor: 'rgba(0, 0, 0, .1)',
+    fontBold: true,
+    rotate: 30,
+    padding: [100, 100],
+  })
 }
 
 onMounted(render)
