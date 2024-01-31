0ef4710b34cc1ec861b539664d4271476ff16a87
shoppingzh <xpzheng6@163.com>
2024-01-31 11:53:53 +0800

    feat: echarts, withTheme demo

--- docs/.vitepress/components/echarts/withTheme.vue
+++ docs/.vitepress/components/echarts/withTheme.vue
@@ -1,36 +1,8 @@
 <template>
-  <!-- <div class="text-right">
-    <ElButton type="primary" link @click="popper.settings = true">配置主题</ElButton>
-  </div> -->
-
   <div>
     <div ref="el" class="mt-2 h-[300px]" />
     <div ref="el2" class="mt-2 h-[300px]"></div>
   </div>
-  
-
-  <!-- <ElDrawer v-model="popper.settings" title="配置主题" size="35%" append-to-body modal-class="bg-[#000]/5">
-    <ElTabs v-model="(value1 as any)" tab-position="left">
-      <ElTabPane v-for="item in tabs1" :key="(item.value as any)" :name="(item.value as any)" :label="item.label" class="px-4">
-        <template v-if="value1 === 'axis'">
-          <ElRadioGroup v-model="value2">
-            <ElRadio v-for="item in tabs2" :key="(item.value as string)" :label="item.value">{{ item.label }}</ElRadio>
-          </ElRadioGroup>
-          <ElForm class="mt-4">
-            <ElFormItem label="文字颜色">
-              <ElColorPicker v-model="(axis.axisLabel.color as any)" />
-            </ElFormItem>
-            <ElFormItem label="文字大小">
-              <ElInputNumber v-model="(axis.axisLabel.fontSize as any)" />
-            </ElFormItem>
-            <ElFormItem label="轴线颜色">
-              <ElColorPicker v-model="(axis.axisLine.lineStyle.color as any)" />
-            </ElFormItem>
-          </ElForm>
-        </template>
-      </ElTabPane>
-    </ElTabs>
-  </ElDrawer> -->
 </template>
 
 <script setup lang="ts">
@@ -40,100 +12,15 @@ import { random } from 'lodash';
 import useChart from 'magic-hooks/lib/useChart'
 import useSelect from 'magic-hooks/lib/useSelect'
 import { computed, onMounted, reactive, ref } from 'vue';
+import theme from './theme.json'
 
 const dimensions = ['安徽省', '广东省', '北京市', '上海市', '陕西省', '辽宁省']
-const popper = reactive({
-  settings: false,
-})
 function generateData() {
   return dimensions.map(o => ({ name: o, value: random(0, 1000) }))
 }
 const barData = ref(generateData())
 const lineData = ref(generateData())
 const pieData = ref(generateData())
-
-const theme = reactive<Theme>({
-  color: ['#00bbf9', '#f15bb5', '#9b5de5', '#fee440', '#8ac926', '#ff006e'],
-  backgroundColor: 'rgba(0, 0, 0, 0.025)',
-  title: {
-    backgroundColor: 'rgba(0, 0, 0, .4)',
-    textStyle: {
-      color: '#fff',
-      fontSize: 16,
-      fontWeight: 'lighter',
-    }
-  },
-  axis: {
-    category: {
-      axisLabel: {
-        color: 'orange',
-        fontSize: 14,
-      },
-      axisLine: {
-        lineStyle: {
-        }
-      }
-    },
-    value: {
-      axisLabel: {
-        color: 'red',
-        fontSize: 12,
-      },
-      axisLine: {
-        lineStyle: {
-          
-        }
-      }
-    }
-  },
-  series: {
-    bar: {
-      barWidth: 20,
-      itemStyle: {
-        borderRadius: [5, 5, 0, 0],
-      }
-    },
-    line: {
-      smooth: true,
-    },
-    pie: {
-      label: {
-        color: '#9b5de5'
-      }
-    }
-  },
-  legend: {
-    plain: {
-      itemWidth: 10,
-      itemHeight: 10,
-    },
-    scroll: {
-      itemWidth: 5,
-      itemHeight: 5,
-    },
-  },
-})
-const { activeValue: value1, items: tabs1 } = useSelect({
-  items: [{
-    value: 'axis',
-    label: '坐标轴',
-  }, {
-    value: 'series',
-    label: '图表系列'
-  }],
-  autoSelect: true,
-})
-const { activeValue: value2, items: tabs2 } = useSelect({
-  items: [{
-    value: 'category',
-    label: '分类轴'
-  }, {
-    value: 'value',
-    label: '值轴',
-  }],
-  autoSelect: true,
-})
-const axis = computed(() => theme.axis[value2.value as keyof Theme['axis']])
 
 const option = computed(() => {
   const option = withTheme({
